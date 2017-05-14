30259e26962a1948040fff3706269deb2023b602
wjt <wjthornberry@gmail.com>
2017-05-14 15:52:37 -0500

    Add pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+//Default scores:
+//Wins: 0
+//Losses: 0
+//Guesses Left: 9
+//Your Guesses so far:
+
+//Set up User 1
+
+//User presses a LETTER key (onkeyup)
+//Will need a line if they press something else 
+//that creates an alert that notifies the user
+
+//If user presses a letter (e.g., 'h') that the computer was
+//not "thinking" of:
+//"Your Guesses so far" adds the character 'h'
+//"Guesses left" decreases by 1 (to 8)
+
+//When the value of "Guesses Left" = 0:
+//the value for "Losses" increases by 1
+//the value for "Guesses Left" returns to default of 9
+//the game "chooses" a new letter
+
+//When the user presses a letter (e.g., 'c') that the computer was
+//"thinking" of:
+//Wins: value increases by 1
+//Computer chooses a new letter
+
