8f7c1d5df6b2e90861de8f447f94e3b88f05098b
Jake Thornberry <jakethornberry@Jakes-MacBook-Pro.local>
2017-05-15 17:31:23 -0500

    Add if else

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,13 +12,32 @@ var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l
 //Grabs user's keystrokes
 //Might need a line if they press something else 
 //that creates an alert
-document.onkeyup = function(event) {
+document.onkeyup = function(e) {
 	
-	var guessedLetters = String.fromCharCode(event.keyCode).toLowerCase();
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+	console.log(userGuess);
 
 //3) Computer "picks" a letter
-	var computerGuess = options[Math.floor(Math.random()*options.length)];
+	var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
+	
+	console.log(computerGuess);
+
+	if (userGuess===computerGuess) {
+		wins++ && guessesLeft--;
+		//for testing; take out after successfully built
+		alert("wins: " + wins);
 	}
+	if (userGuess!==computerGuess) {
+		losses++ && guessesLeft--;
+		//for testing; take out after successfully built
+		alert("losses: " + losses);
+	} else {
+		alert("Oops! You didn't type a letter. Wanna try that again?");
+	}
+
+
+	
 }
 
 
@@ -26,16 +45,18 @@ document.onkeyup = function(event) {
 //not "thinking" of:
 //"Your Guesses so far" adds the character 'h'
 //"Guesses left" decreases by 1 (to 8)
+//console.log everything during testing ex: alert("wins: " + wins);
+//but take out after testing
 
 //When the value of "Guesses Left" = 0:
-//the value for "Losses" increases by 1
+//the value for "Losses" increases by 1 losses++ in loop
 //the value for "Guesses Left" returns to default of 9
 //the game restarts - without refreshing the page 
 //and the game "chooses" a new letter
 
 //When the user presses a letter (e.g., 'c') that the computer was
 //"thinking" of:
-//Wins: value increases by 1
+//Wins: value increases by 1 wins++ in loop
 //Game starts over (without refreshing page)
 //Computer chooses a new letter
 
