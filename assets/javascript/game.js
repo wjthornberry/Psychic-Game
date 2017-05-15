1975a6e00a560bda67616f8a5af288fb275b8e82
Jake Thornberry <jakethornberry@dhcp-10-105-88-229.wireless.northwestern.private>
2017-05-15 16:07:26 -0500

    Add random computer guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,7 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var guessedLetters = [];
+
 //This array holds the letters A-Z that the computer will randomly choose from.
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
@@ -12,12 +13,13 @@ var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l
 //Might need a line if they press something else 
 //that creates an alert
 document.onkeyup = function(event) {
-	guessesLeft--;
+	
 	var guessedLetters = String.fromCharCode(event.keyCode).toLowerCase();
-}
 
-//This function will pick the letter
-function chooseLetter
+//3) Computer "picks" a letter
+	var computerGuess = options[Math.floor(Math.random()*options.length)];
+	}
+}
 
 
 //If user presses a letter (e.g., 'h') that the computer was
